57b5c65006362c6e08a02d4f41d958f72b4722ba
jossephalvarez <josseph.alvarez@melia.com>
2025-02-10 17:40:53 +0100

    UserDetailPage updated with useFetch

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,21 +1,13 @@
 // app/users/[id]/page.tsx
 'use client';
-import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation'; // Usar useParams de next/navigation
 import { User } from '@/types/User';
-
+import {useFetch} from "@/hooks/useFetch";
+import { USERS_API } from '@/constants/api';
 const UserDetailPage: React.FC = () => {
     const { id } = useParams();  // Utilizamos useParams para obtener el id de la URL
-    const [user, setUser] = useState<User | null>(null);
+    const { data: user, loading, error } = useFetch<User>(`${USERS_API}/${id}`);
 
-    useEffect(() => {
-        if (id) {
-            // Simulamos una llamada a la API para obtener los detalles del usuario
-            fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-                .then((response) => response.json())
-                .then((data) => setUser(data));
-        }
-    }, [id]);
 
     if (!user) {
         return <div>Cargando...</div>;
@@ -23,10 +15,10 @@ const UserDetailPage: React.FC = () => {
 
     return (
         <div>
-            <h2>Detalles de {user.name}</h2>
-            <p>Email: {user.email}</p>
-            <p>Teléfono: {user.phone}</p>
-            <p>Dirección: {user.address.street}, {user.address.city}</p>
+            <h2>Detalles de {user?.name}</h2>
+            <p>Email: {user?.email}</p>
+            <p>Teléfono: {user?.phone}</p>
+            <p>Dirección: {user.address?.street}, {user.address?.city}</p>
         </div>
     );
 };
